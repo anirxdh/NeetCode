d9ff5e6687f221df5fb03509667c452a2ef52e9b
Anirudh Vasudevan <anirudh@Anirudhs-MacBook-Pro-4.local>
2025-04-01 20:05:43 -0500

    RemoveDuplicatesFromSortedArray code added

---
+++ Arrays/ContainsDuplicate.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasDuplicate(self, nums: list[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+    
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.hasDuplicate([1,py 2, 3, 1])) # True
+    print(s.hasDuplicate([1, 2, 3, 4])) # False
+    print(s.hasDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])) # True
